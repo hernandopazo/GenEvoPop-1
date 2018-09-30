7cdfd68e9b446e1e75759e4316c8344343901f4b
D.N. De Panis <9466288+lunfardista@users.noreply.github.com>
2018-09-30 19:03:56 -0300

    Update abyss_scripts.sh

--- TP4/abyss_scripts.sh
+++ TP4/abyss_scripts.sh
@@ -1,3 +1,4 @@
+```
 #!/bin/bash
 
 ## Script para ejecutar los ejemplos del TP4 con ABySS assembler
@@ -13,7 +14,7 @@ for i in $(seq 29 2 47)
         do
                 echo -._.-._.-._.-._.-._.-._.-._.-._.-._.-._.- k = "$i" -._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-
                 echo
-               /home/estudiante/software/abyss-2.1.0/bin/abyss-pe -C /home/estudiante/TP4/assemblies/mitoSINGLE/abyss/k"$i" k="$i" name=Hsap_mitoSE.k"$i" se=/home/estudiante/TP4/reads/GA2_DRR001063_redset.fastq.gz
+                abyss-pe -C /home/estudiante/TP4/assemblies/mitoSINGLE/abyss/k"$i" k="$i" name=Hsap_mitoSE.k"$i" se=/home/estudiante/TP4/reads/GA2_DRR001063_redset.fastq.gz
                 echo
                 echo
 done
@@ -27,7 +28,7 @@ done
 #        do
 #                echo -._.-._.-._.-._.-._.-._.-._.-._.-._.-._.- k = "$i" -._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-
 #                echo
-#               /home/estudiante/software/abyss-2.1.0/bin/abyss-pe -C /home/estudiante/TP4/assemblies/mitoPAIRED/abyss/k"$i" k="$i" name=Hsap_mitoPE.k"$i" in=\'/home/estudiante/TP4/reads/MISEQ_SRR2075910_trim_1p.fastq.gz /home/estudiante/TP4/reads/MISEQ_SRR2075910_trim_2p.fastq.gz\' se=\'/home/estudiante/TP4/reads/MISEQ_SRR2075910_trim_u.fastq.gz'
+#                abyss-pe -C /home/estudiante/TP4/assemblies/mitoPAIRED/abyss/k"$i" k="$i" name=Hsap_mitoPE.k"$i" in='/home/estudiante/TP4/reads/MISEQ_SRR2075910_trim_1p.fastq.gz /home/estudiante/TP4/reads/MISEQ_SRR2075910_trim_2p.fastq.gz' se='/home/estudiante/TP4/reads/MISEQ_SRR2075910_trim_u.fastq.gz'
 #                echo
 #                echo
 #done
@@ -41,7 +42,8 @@ done
 #	do
 #		echo -._.-._.-._.-._.-._.-._.-._.-._.-._.-._.- k = "$i" -._.-._.-._.-._.-._.-._.-._.-._.-._.-._.-
 #		echo
-#		/home/estudiante/software/abyss-2.1.0/bin/abyss-pe -C /home/estudiante/TP4/assemblies/mitoPAIRED/abyss/k"$i" k="$i" name=Saur.k"$i" in=\'/home/estudiante/TP4/reads/sim_Staur_1.fq.gz /home/estudiante/TP4/reads/sim_Staur_2.fq.gz\'
+#		abyss-pe -C /home/estudiante/TP4/assemblies/mitoPAIRED/abyss/k"$i" k="$i" name=Saur.k"$i" in='/home/estudiante/TP4/reads/sim_Staur_1.fq.gz /home/estudiante/TP4/reads/sim_Staur_2.fq.gz'
 #		echo
 #		echo
 #done
+```
